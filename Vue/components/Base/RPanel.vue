b6a1b26a7080d4d0e1a5f39298231b1a8827ba07
Tianjian <mail@isdtj.com>
2023-05-08 13:07:24 +0800

    复制已有组件

---
+++ Vue/components/Base/RPanel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="panel">
+    <div class="panel-header">
+      <div class="panel-header-title">
+        <slot name="title" />
+        {{ title }}
+      </div>
+      <div class="panel-heaer-extra">
+        <slot name="extra" />
+      </div>
+    </div>
+    <div class="panel-body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  title: String
+});
+</script>
+
+<style scoped>
+.panel {
+  padding: var(--r-panel-padding);
+  background-color: var(--r-panel-background-color);
+  border-radius: var(--r-border-radius);
+  box-sizing: border-box;
+}
+
+.panel-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.panel-header-title {
+  font-size: 16px;
+  color: var(--font-color_1);
+  font-weight: bold;
+}
+
+.panel-body {
+  margin-top: 10px;
+}
+</style>
---
+++ Vue/components/Naive/NaiveConfig.vue
@@ -0,0 +1,20 @@
+<template>
+  <n-config-provider :theme-overrides="themeOverrides">
+    <n-dialog-provider>
+      <n-message-provider>
+        <slot />
+      </n-message-provider>
+    </n-dialog-provider>
+  </n-config-provider>
+</template>
+
+<script lang="ts" setup>
+import { GlobalThemeOverrides } from 'naive-ui';
+
+withDefaults(defineProps<{
+  themeOverrides?: GlobalThemeOverrides
+}>(), {})
+
+</script>
+
+<style scoped></style>
