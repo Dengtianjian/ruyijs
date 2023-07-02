b50243ae9704468960d46659ced942b71dd2fe42
Tianjian <mail@isdtj.com>
2023-07-02 23:25:41 +0800

    优化状态存储

--- MiniProgram/foundation/MPStore.ts
+++ MiniProgram/foundation/MPStore.ts
@@ -15,60 +15,51 @@ export default class MPStore<T extends Record<string | number, any>> {
     if (!Page) return null;
     return Page['_RuyiPageId'].toString();
   }
-  set(key: keyof T, value: any, callback?: () => void) {
+  #updatePages(key: string, callback?: () => void, value: any = null) {
+    key = key.toString();
     const Pages = getCurrentPages();
 
-    this.#data[key.toString()] = value;
-    if (this.#links.has(key.toString())) {
-      const links = this.#links.get(key.toString());
+    if (this.#links.has(key)) {
+      const links = this.#links.get(key);
       Pages.forEach(item => {
         if (item['_RuyiPageId'] && links.has(item['_RuyiPageId'])) {
-          links.get(item['_RuyiPageId'].toString()).forEach(AttributeName => {
+          links.get(item['_RuyiPageId']).forEach(AttributeName => {
             item.setData({
-              [AttributeName.toString()]: value
+              [AttributeName.toString()]: value ?? this.#data[key]
             }, callback)
           });
         }
       });
     }
+  }
+  set(key: keyof T, value: any, callback?: () => void) {
+    this.#data[key.toString()] = value;
+
+    this.#updatePages(key.toString(), callback)
     return this;
   }
   setData(key: keyof T, value: any, callback?: () => void) {
-    const Pages = getCurrentPages();
 
-    if (this.#links.has(key.toString())) {
-      const links = this.#links.get(key.toString());
-      Pages.forEach(item => {
-        if (item['_RuyiPageId'] && links.has(item['_RuyiPageId'])) {
-          links.get(item['_RuyiPageId'].toString()).forEach(AttributeName => {
-            item.setData({
-              [AttributeName.toString()]: value
-            }, callback)
-          });
-        }
-      });
-    }
+    this.#updatePages(key.toString(), callback, value);
     return this;
   }
   link(key: string, storeKey: keyof T = null) {
     const PageId = this.#getPageId();
+    if (storeKey === null) {
+      storeKey = key.toString();
+    }
 
     if (PageId) {
       if (storeKey) {
         if (!this.#links.has(storeKey)) {
-          this.#links.set(storeKey.toString(), new Map());
-          this.#links.get(storeKey.toString()).set(PageId, []);
+          this.#links.set(storeKey, new Map());
+          this.#links.get(storeKey).set(PageId, []);
         }
 
-        this.#links.get(storeKey.toString()).get(PageId).push(key);
-      } else {
-        if (!this.#links.has(key)) {
-          this.#links.set(key.toString(), new Map());
-          this.#links.get(key.toString()).set(PageId, []);
-        }
-
-        this.#links.get(key.toString()).get(PageId).push(key);
+        this.#links.get(storeKey).get(PageId).push(key);
       }
+
+      this.#updatePages(storeKey.toString());
     }
 
     return this;
