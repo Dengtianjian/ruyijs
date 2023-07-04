07b1d3f3dacb55f86f20ab5f07dbd41c49b12927
Tianjian <mail@isdtj.com>
2023-07-04 23:12:06 +0800

    空组件

---
+++ MiniProgram/components/REmpty/REmpty.ts
@@ -0,0 +1,86 @@
+// Ruyi/MiniProgram/components/REmpty/REmpty.ts
+Component<{
+
+}, {
+  icon: {
+    type: StringConstructor,
+    value: string
+  },
+  useIconSlot: {
+    type: BooleanConstructor,
+    value: boolean
+  },
+  title: {
+    type: StringConstructor,
+    value: string
+  },
+  useTitleSlot: {
+    type: BooleanConstructor,
+    value: boolean
+  },
+  description: {
+    type: StringConstructor,
+    value: string
+  },
+  useDescriptionSlot: {
+    type: BooleanConstructor,
+    value: boolean
+  },
+  show: {
+    type: BooleanConstructor,
+    value: boolean
+  }
+}, {
+
+  }>({
+    options: {
+      multipleSlots: true
+    },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+      icon: {
+        type: String,
+        value: null
+      },
+      useIconSlot: {
+        type: Boolean,
+        value: false
+      },
+      title: {
+        type: String,
+        value: ""
+      },
+      useTitleSlot: {
+        type: Boolean,
+        value: false
+      },
+      description: {
+        type: String,
+        value: ""
+      },
+      useDescriptionSlot: {
+        type: Boolean,
+        value: false
+      },
+      show: {
+        type: Boolean,
+        value: true
+      }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+  })
