c8117418d798c4543bdf96b4158eebc0b73e5c7a
DengTianjian <mail@isdtj.com>
2023-09-15 00:03:07 +0800

    覆写http的请求方法

--- Vue/foundation/HTTP/request.ts
+++ Vue/foundation/HTTP/request.ts
@@ -53,6 +53,58 @@ export default class Request extends HTTP {
       return res.data;
     });
   }
+  get<ResponseData>(uri: string | string[], query: Record<string, number | string> = null) {
+    if (query) {
+      for (const key in query) {
+        this.query(key, query[key]);
+      }
+    }
+    return this.send<ResponseData>(uri, "GET");
+  }
+  /**
+  * 发送POST请求
+  * @param uri URI
+  * @returns Promise
+  */
+  post<ResponseData>(uri: string | string[], bodyData: TBody = null) {
+    if (bodyData) {
+      this.body(bodyData);
+    }
+    return this.send<ResponseData>(uri, "POST");
+  }
+  /**
+   * 发送PUT请求
+   * @param uri URI
+   * @returns Promise
+   */
+  put<ResponseData>(uri: string | string[], bodyData: TBody = null) {
+    if (bodyData) {
+      this.body(bodyData);
+    }
+    return this.send<ResponseData>(uri, "PUT");
+  }
+  /**
+   * 发送delete请求
+   * @param uri URI
+   * @returns Promise
+   */
+  delete<ResponseData>(uri: string | string[], bodyData: TBody = null) {
+    if (bodyData) {
+      this.body(bodyData);
+    }
+    return this.send<ResponseData>(uri, "DELETE");
+  }
+  /**
+   * 发送patch请求
+   * @param uri URI
+   * @returns Promise
+   */
+  patch<ResponseData>(uri: string | string[], bodyData: TBody = null) {
+    if (bodyData) {
+      this.body(bodyData);
+    }
+    return this.send<ResponseData>(uri, "PATCH");
+  }
   /**
    * 上传文件
    * @param uri URI
