231e1bca72dfcfcebe18371d5ebbde6e0cfeb3ae
DengTianjian <mail@isdtj.com>
2023-10-07 10:24:35 +0800

    dzx设置项表单类增加数据处理

--- Vue/services/SettingFormService.ts
+++ Vue/services/SettingFormService.ts
@@ -1,8 +1,5 @@
-import { reactive, ref } from "vue"
-import Request from "../foundation/HTTP/Request";
+import { UnwrapNestedRefs, reactive, ref, toRaw } from "vue"
 import SettingsApi from "../api/common/SettingsApi";
-
-
 
 export default class <T extends object> {
   loading = ref<boolean>(false);
@@ -11,21 +8,23 @@ export default class <T extends object> {
   // @ts-ignore
   settings = reactive<T>({});
 
-  load(keys: string[]) {
+  load(keys: string[]): Promise<T> {
     this.loading.value = true;
+    // @ts-ignore
     return SettingsApi.list<T>(keys).then(settings => {
       for (const key in settings.data) {
         // @ts-ignore
         this.settings[key] = settings.data[key];
       }
+      return settings;
     }).finally(() => {
       this.loading.value = false;
     });
   }
-  save() {
+  save(dataHandle: (data?: UnwrapNestedRefs<T>) => T) {
     this.saving.value = true;
     this.disabled.value = true;
-    return SettingsApi.saveList(this.settings).finally(() => {
+    return SettingsApi.saveList(dataHandle ? dataHandle(toRaw(this.settings)) : this.settings).finally(() => {
       this.saving.value = false;
       this.disabled.value = false;
     });
--- Vue/services/discuzX/DiscuzXSettingFormService.ts
+++ Vue/services/discuzX/DiscuzXSettingFormService.ts
@@ -1,25 +1,26 @@
-import { reactive, ref } from "vue"
+import { UnwrapNestedRefs, reactive, ref } from "vue"
 import DiscuzXSettingsApi from "../../api/discuzX/common/DiscuzXSettingsApi";
 import SettingFormService from "../SettingFormService";
 
 export default class <T extends Record<string, any>> extends SettingFormService<T> {
 
-  load(keys: string[]) {
+  load(keys: string[]): Promise<T> {
     this.loading.value = true;
     return DiscuzXSettingsApi.list<T>(keys).then(settings => {
       for (const key in settings) {
         // @ts-ignore
         this.settings[key] = settings[key];
       }
+      return settings;
     }).finally(() => {
       this.loading.value = false;
     });
   }
-  // @ts-ignore
-  save() {
+
+  save(dataHandle: (data?: UnwrapNestedRefs<T>) => T): Promise<any> {
     this.saving.value = true;
     this.disabled.value = true;
-    return DiscuzXSettingsApi.saveList(this.settings).finally(() => {
+    return DiscuzXSettingsApi.saveList(dataHandle ? dataHandle(this.settings) : this.settings).finally(() => {
       this.saving.value = false;
       this.disabled.value = false;
     });
