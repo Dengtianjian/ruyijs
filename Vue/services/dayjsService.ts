c22b0dd8d9655481c187e22588d1160d0f30da0b
DengTianjian <mail@isdtj.com>
2023-10-28 16:14:31 +0800

    增加增强dayjs方法

--- Vue/services/dayjsService.ts
+++ Vue/services/dayjsService.ts
@@ -6,5 +6,13 @@ export default {
   init() {
     dayjs.locale("zh-cn");
     dayjs.extend(relativeTime);
+  },
+  formatAutoFromNow(date?: string | number | Date | dayjs.Dayjs, toFromNowThreshold: number = 1000 * 60 * 60 * 24, formatTemplate: string = 'YYYY-MM-DD HH:mm:ss', withoutSuffix?: boolean) {
+    const d = dayjs(date);
+    if (Date.now() - d.valueOf() > toFromNowThreshold) {
+      return d.format(formatTemplate);
+    }
+
+    return d.fromNow(withoutSuffix);
   }
 }
