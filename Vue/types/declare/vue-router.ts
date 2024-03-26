ee44ef482b31f276ac942390084d308e9dda56da
DengTianjian <mail@isdtj.com>
2024-03-26 17:31:54 +0800

    增加路由菜单的 TS 声明

---
+++ Vue/types/declare/vue-router.ts
@@ -0,0 +1,12 @@
+import { VNode, VNodeChild } from 'vue';
+
+export type TNaiveMenuRouteMetaMenuOption = {
+  index?: number,
+  disabled?: boolean,
+  extra?: string | (() => VNodeChild),
+  icon?: () => VNode,
+  label?: string | (() => VNodeChild),
+  show?: boolean,
+  type?: "group",
+  level?: number
+}
