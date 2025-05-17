855b3ddd171471dafa33f557bf744eae107c9f4e
DengTianjian <mail@isdtj.com>
2025-05-17 16:41:43 +0800

    上传文件时判断是否仅上传,不是的话就 push 文件信息到文件列表

--- Vue/components/Naive/RNaiveUpload.vue
+++ Vue/components/Naive/RNaiveUpload.vue
@@ -1,49 +1,64 @@
 <template>
-  <n-upload :list-type="listType" :max="Max" :file-list="FileList" @change="uploadFile" @remove="removeFile"
-    v-bind="$attrs">
+  <n-upload
+    :list-type="listType"
+    :max="Max"
+    :file-list="FileList"
+    @change="uploadFile"
+    @remove="removeFile"
+    v-bind="$attrs"
+  >
     <slot />
   </n-upload>
 </template>
 
 <script lang="ts" setup>
-import { UploadFileInfo, useMessage } from 'naive-ui';
-import { reactive, ref, watch } from 'vue';
+import { UploadFileInfo, useMessage } from "naive-ui";
+import { reactive, ref, watch } from "vue";
 
 const NMessage = useMessage();
 
-const Props = withDefaults(defineProps<{
-  files?: Array<UploadFileInfo>,
-  file?: UploadFileInfo,
-  single?: boolean,
-  uploadFile: (file: UploadFileInfo) => Promise<string | UploadFileInfo>
-  removeFile?: (file: UploadFileInfo) => Promise<boolean>,
-  max?: number
-  listType?: "text" | "image" | "image-card",
-  onlyUpload?: boolean
-}>(), {
-  files: null,
-  file: null,
-  single: false,
-  max: 1,
-  listType: "text",
-  onlyUpload: true
-});
+const Props = withDefaults(
+  defineProps<{
+    files?: Array<UploadFileInfo>;
+    file?: UploadFileInfo;
+    single?: boolean;
+    uploadFile: (file: UploadFileInfo) => Promise<string | UploadFileInfo>;
+    removeFile?: (file: UploadFileInfo) => Promise<boolean>;
+    max?: number;
+    listType?: "text" | "image" | "image-card";
+    onlyUpload?: boolean;
+  }>(),
+  {
+    files: null,
+    file: null,
+    single: false,
+    max: 1,
+    listType: "text",
+    onlyUpload: true,
+  }
+);
 
 const Max = ref<number>(Props.max);
-watch(() => Props.max, newV => {
-  if (Props.single) {
-    Max.value = 1;
-  } else {
-    Max.value = newV;
+watch(
+  () => Props.max,
+  (newV) => {
+    if (Props.single) {
+      Max.value = 1;
+    } else {
+      Max.value = newV;
+    }
   }
-});
-watch(() => Props.single, newV => {
-  if (newV) {
-    Max.value = 1;
-  } else {
-    Max.value = Props.max;
+);
+watch(
+  () => Props.single,
+  (newV) => {
+    if (newV) {
+      Max.value = 1;
+    } else {
+      Max.value = Props.max;
+    }
   }
-});
+);
 
 const FileList = reactive([]);
 if (!Props.onlyUpload && Props.single) {
@@ -56,90 +71,115 @@ if (!Props.onlyUpload && Props.single) {
 
 const Emits = defineEmits(["update:file", "update:files"]);
 
-function uploadFile({ file }: { file: UploadFileInfo, fileList: Array<UploadFileInfo>, event?: Event }) {
+function uploadFile({
+  file,
+}: {
+  file: UploadFileInfo;
+  fileList: Array<UploadFileInfo>;
+  event?: Event;
+}) {
   if (file.file) {
-    Props.uploadFile(file).then(res => {
-      if (typeof res === "string") {
-        if (Props.single === true || Props.single === undefined) {
-          if (Props.onlyUpload) {
-            Emits("update:file", {
-              id: file.id,
-              name: file.name,
-              status: "finished",
-              url: res
-            });
+    Props.uploadFile(file)
+      .then((res) => {
+        if (typeof res === "string") {
+          if (Props.single === true || Props.single === undefined) {
+            if (Props.onlyUpload) {
+              Emits("update:file", {
+                id: file.id,
+                name: file.name,
+                status: "finished",
+                url: res,
+              });
+            } else {
+              FileList.splice(0, 1, {
+                id: file.id,
+                name: file.name,
+                status: "finished",
+                url: res,
+              });
+              Emits("update:file", FileList[0]);
+            }
           } else {
-            FileList.splice(0, 1, {
-              id: file.id,
-              name: file.name,
-              status: "finished",
-              url: res
-            });
-            Emits("update:file", FileList[0]);
+            if (!Props.onlyUpload) {
+              FileList.push({
+                id: file.id,
+                name: file.name,
+                status: "finished",
+                url: res,
+              });
+            }
+
+            Emits("update:files", FileList);
           }
         } else {
-          FileList.push({
-            id: file.id,
-            name: file.name,
-            status: "finished",
-            url: res
-          });
-          Emits("update:files", FileList);
+          if (Props.single === true || Props.single === undefined) {
+            !Props.onlyUpload && FileList.splice(0, 1, res);
+            Emits("update:file", res);
+          } else {
+            if (!Props.onlyUpload) {
+              FileList.push(res);
+            }
+            Emits("update:files", FileList);
+          }
         }
-      } else {
-        if (Props.single === true || Props.single === undefined) {
-          !Props.onlyUpload && FileList.splice(0, 1, res);
-          Emits("update:file", res);
-        } else {
-          Emits("update:files", FileList);
-        }
-      }
-    }).catch(err => {
-      NMessage.error(err.message ?? "上传失败，请稍后重试");
-    })
+        console.log(FileList);
+      })
+      .catch((err) => {
+        NMessage.error(err.message ?? "上传失败，请稍后重试");
+      });
   }
 }
 
 function removeFile({ file }: { file: UploadFileInfo }) {
-
   return new Promise((resolve, reject) => {
     if (Props.removeFile) {
       Props.removeFile(file).then(resolve).catch(reject);
     } else {
       resolve(true);
     }
-  }).then(() => {
-    FileList.splice(FileList.findIndex(item => {
-      return item.id === file.id;
-    }), 1);
-    if (Props.single) {
-      Emits("update:file", null);
-    } else {
-      Emits("update:files", FileList);
-    }
-  }).catch(err => {
-    if (err.statusCode === 404) {
-      FileList.splice(FileList.findIndex(item => {
-        return item.id === file.id;
-      }), 1);
+  })
+    .then(() => {
+      FileList.splice(
+        FileList.findIndex((item) => {
+          return item.id === file.id;
+        }),
+        1
+      );
       if (Props.single) {
         Emits("update:file", null);
       } else {
         Emits("update:files", FileList);
       }
-      return true;
-    }
-    NMessage.error(err.message ?? "删除失败，请稍后重试");
-    return false;
-  });
+    })
+    .catch((err) => {
+      if (err.statusCode === 404) {
+        FileList.splice(
+          FileList.findIndex((item) => {
+            return item.id === file.id;
+          }),
+          1
+        );
+        if (Props.single) {
+          Emits("update:file", null);
+        } else {
+          Emits("update:files", FileList);
+        }
+        return true;
+      }
+      NMessage.error(err.message ?? "删除失败，请稍后重试");
+      return false;
+    });
 }
 
-watch(() => Props.file, newV => {
-  FileList.splice(0);
-  if (newV) {
-    FileList.push(Props.file);
+watch(
+  () => Props.file,
+  (newV) => {
+    FileList.splice(0);
+    if (newV) {
+      FileList.push(Props.file);
+    }
   }
-});
+);
 </script>
 
 <style scoped></style>
